f6ae226846b3a723382ab7c4354db6ae8721df23
Stephen-Agbeyebiawo <stephen.agbeyebiawo@gmail.com>
2023-05-15 21:46:01 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">African Store</a>
+      </header>
+      <main>
+        list of products
+      </main>
+    </div>
+  );
+}
+
+export default App;
